ff608e3f67963d6b930511a5fdbd7eb111b18d29
Ilshat Gelmutdinov <gelmutdinov1988@mail.ru>
2024-12-08 22:20:32 +0300

    My first commit for Halal_English_Practice_App

---
+++ app/src/main/java/com/example/halalenglishpracticeapp/ItemRowModel.kt
@@ -0,0 +1,7 @@
+package com.example.halalenglishpracticeapp
+
+data class ItemRowModel(
+    val imageId: Int,
+    val title: String,
+    val content: String
+)
---
+++ app/src/main/java/com/example/halalenglishpracticeapp/MainActivity.kt
@@ -0,0 +1,67 @@
+package com.example.halalenglishpracticeapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+
+            PeopleScreen()
+
+        }
+    }
+}
+
+
+
+@Composable
+fun PeopleScreen() {
+
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.Gray)
+    ) {
+        itemsIndexed(
+            listOf(
+                ItemRowModel(R.drawable.java1, "qwerty", "Lorem ipsum dolor sit" +
+                        " amet, consectetuer adipiscing elit. Aenean commodo ligula eget" +
+                        " dolor. Aenean massa."),
+                ItemRowModel(R.drawable.java2, "uiop", "test"),
+                ItemRowModel(R.drawable.java3, "asdfg", "test"),
+                ItemRowModel(R.drawable.java4, "hjk", "test"),
+                ItemRowModel(R.drawable.kzn, "qwe", "test"),
+                ItemRowModel(R.drawable.kotlin, "tyqwer", "test"),
+                ItemRowModel(R.drawable.ubuntu, "qert", "test"),
+                ItemRowModel(R.drawable.java2, "uiop", "test"),
+                ItemRowModel(R.drawable.java3, "asdfg", "test"),
+                ItemRowModel(R.drawable.java4, "hjk", "test"),
+                ItemRowModel(R.drawable.kzn, "qwe", "test"),
+                ItemRowModel(R.drawable.kotlin, "tyqwer", "test"),
+                ItemRowModel(R.drawable.ubuntu, "qert", "test")
+            )
+        ) { index, item ->
+            MyRow(item = item)
+        }
+    }
+
+}
+
+
